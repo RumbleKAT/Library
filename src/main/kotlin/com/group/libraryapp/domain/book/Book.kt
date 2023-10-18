c7600d73a38a085d1b27784b9fdf97046dbf2d2f
RumbleKAT <reki318@naver.com>
2023-10-18 22:00:16 +0900

    (feat) add EnumType

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -1,21 +1,36 @@
 package com.group.libraryapp.domain.book
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 class Book (
     val name:String,
 
+    @Enumerated(EnumType.STRING) // DB 들어갈때 string으로 들어감
+    val type:BookType,
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
 ){
     init{
         if(name.isBlank()){
             throw IllegalArgumentException("이름은 비어 있을 수 없습니다.")
+        }
+    }
+    // 테스트 코드에 영향이 없다. object mother pattern
+    companion object {
+        fun fixture(
+            name: String = "책 이름",
+            type: BookType = BookType.COMPUTER,
+            id: Long? = null,
+        ): Book{
+            return Book(
+                name = name,
+                type = type,
+                id = id,
+            )
         }
     }
 }
---
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookType.kt
@@ -0,0 +1,13 @@
+package com.group.libraryapp.domain.book
+
+enum class BookType {
+    COMPUTER,
+    ECONOMY,
+    SOCIETY,
+    LANGUAGE,
+    SCIENCE
+}
+
+/*
+* 기존 Enum의 순서가 바뀌면 안됨
+* */
